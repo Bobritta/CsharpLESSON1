733a54d3374bc285ff177b286c572bdd7abb16b6
Tanya <hummingbird_t@mail.ru>
2024-04-22 09:59:00 +0300

    made comparison between five  values

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 4;
+int b = 60;
+int c = 9;
+int d = 2;
+int e = 17;
+
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+
+if (c>max)
+{
+    max = c;
+}
+
+if (d>max)
+{
+    max = d;
+}
+
+if (e>max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
